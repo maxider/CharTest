5c69bb27ab75eb3b3514809246cada033fa2543c
maxider <maxweilandt1998@googlemail.com>
2018-07-13 17:41:38 +0200

    Initial commit

---
+++ Source/CharTest/CharTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CharTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARTEST_API ACharTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
